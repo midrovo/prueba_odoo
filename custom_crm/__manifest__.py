b3c8f00cb1d94fb217f2fecb956e426a8e0f7508
midrovo <midrovo96@outlook.com>
2024-07-07 20:57:18 -0500

    fix-add: Creando modulo custom crm iniciando modelo vista controlador

---
+++ custom_crm/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Custom CRM",
+
+    "summary": "Modulo CRM para la gestion de visitas",
+
+    "description": "Modulo CRM para la gestion de visitas...",
+
+    "author": "midrovo",
+    "website": "http://www.odoo.com",
+
+    "category": "Uncategorized",
+    "version": "0.1",
+
+    "depends": ["base"],
+
+    "data": [
+        "views/views.xml",
+        "views/templates.xml"
+    ]
+}
