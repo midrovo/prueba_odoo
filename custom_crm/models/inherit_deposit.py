44c5bda9a96fc7a466413693d6d398238a1f78d0
midrovo <midrovo96@outlook.com>
2024-07-09 23:24:56 -0500

    fix-update: Actualizando archivos

--- custom_crm/models/inherit_deposit.py
+++ custom_crm/models/inherit_deposit.py
@@ -4,5 +4,11 @@ class Deposit(models.Model):
     _inherit = 'account.payment'
 
     is_internal_transfer = fields.Boolean(readonly=True)
-    payment_type = fields.Selection(readonly=False, required=False, default='outbound')
+    # payment_type = fields.Selection(readonly=False, required=False, default='outbound')
     partner_bank_id = fields.Many2one(comodel_name='res.partner.bank', invisible=True, readonly=False)
+
+    @api.model
+    def default_get(self, fields_list):
+        res = super(Deposit, self).default_get(fields_list)
+        res['payment_type'] = 'outbound'
+        return res
