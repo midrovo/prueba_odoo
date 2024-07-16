6580df1854954a09f06e042ce6dc523a3b421b38
midrovo <midrovo96@outlook.com>
2024-07-15 22:51:03 -0500

    fix-update: Actualizando archivos

--- custom_crm/models/inherit_deposit.py
+++ custom_crm/models/inherit_deposit.py
@@ -1,17 +1,35 @@
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class Deposit(models.Model):
     _inherit = 'account.payment'
 
-    is_internal_transfer = fields.Boolean(readonly=True)
-    # payment_type = fields.Selection(
-    #     [('inbound', 'Recibir'), ('outbound', 'Enviar')],
-    #     readonly=False,
-    #     required=False,
-    #     default='outbound',
-    #     widget='check')
-    partner_bank_id = fields.Many2one(comodel_name='res.partner.bank', invisible=True, readonly=False)
+    codigo_ref = fields.Char(string='Ref. papeleta')
 
-    @api.onchange('payment_type')
-    def _onchange_payment_type(self):
-        pass
+    nota = fields.Char(string='Nota')
+
+    cuenta_bancaria = fields.Many2one(
+        comodel_name='res.partner.bank',
+        string='Cuenta de banco'
+    )
+
+    payment_method_line_id = fields.Many2one(
+        comodel_name='account.payment.method.line',
+        required=False        
+    )
+
+    journal_id = fields.Many2one(
+        comodel_name='account_journal',
+        required=False        
+    )
+    
+    @api.model
+    def load(self, data):
+        try:
+            for record in data:
+                if 'codigo_ref' in record:
+                    record['codigo_ref'] = 25
+
+                self.create(record)
+
+        except Exception as e:
+            raise exceptions.UserError(f'error al importar datos {str(e)}')
